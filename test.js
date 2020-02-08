4805568f1346069deadbcac12f8264c9bf809482
Antoine Beauvais-Lacasse <antoine.beaulac@gmail.com>
2020-02-07 21:03:41 -0500

    Add encoding test

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 
 const ImageData = require('@canvas/image-data')
 const lodepng = require('lodepng')
+const pixelmatch = require('pixelmatch')
 
 const jpegTurbo = require('./')
 
@@ -23,6 +24,29 @@ describe('JPEG-Turbo', () => {
       assert.strictEqual(result.width, reference.width)
       assert.strictEqual(result.height, reference.height)
       assert.deepStrictEqual(result.data, new Uint8ClampedArray(reference.data))
+    })
+  }
+
+  for (const fixture of fixtures) {
+    it(`encodes a valid JPEG from  ${fixture}.png`, async () => {
+      const referenceSource = fs.readFileSync(`fixtures/${fixture}_ref.png`)
+      const reference = await lodepng.decode(referenceSource)
+
+      const encodedJpeg = jpegTurbo.encode(reference)
+      const result = jpegTurbo.decode(encodedJpeg)
+
+      assert(result instanceof ImageData)
+      assert.strictEqual(result.width, reference.width)
+      assert.strictEqual(result.height, reference.height)
+
+      const differenceCount = pixelmatch(
+        result.data,
+        reference.data,
+        Buffer.alloc(result.data.byteLength),
+        result.width,
+        result.height
+      )
+      assert.strictEqual(differenceCount, 0)
     })
   }
 })
